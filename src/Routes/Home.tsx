4d7a025b5683954caffaa5a9780b7ef5ce6e2647
been94 <rlagywhd019@naver.com>
2024-05-18 19:54:14 +0900

    # home fix, react-query >= 5 update!

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,5 +1,57 @@
+import { IGetMoviesResult, getMovies } from "../api";
+import styled from "styled-components";
+import { makeImgPath } from "./Util";
+import { useQuery } from "@tanstack/react-query";
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  font-size: 30px;
+  width: 50%;
+`;
+
 export default function Home() {
+  const { data, isLoading } = useQuery<IGetMoviesResult>({
+    queryKey: ["movies", "nowPlaying"],
+    queryFn: getMovies,
+  });
+  console.log(data);
   return (
-    <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>home</div>
+    <>
+      <Wrapper>
+        {isLoading ? (
+          <Loader>
+            <span>로딩중</span>
+          </Loader>
+        ) : (
+          <Banner bgPhoto={makeImgPath(data?.results[0].backdrop_path || "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        )}
+      </Wrapper>
+    </>
   );
 }
---
+++ src/api.ts
@@ -0,0 +1,36 @@
+export const API_KEY = "4d61ce9eafcbe91b13a841f060a1ec14";
+const BEARER_KEY =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZDYxY2U5ZWFmY2JlOTFiMTNhODQxZjA2MGExZWMxNCIsInN1YiI6IjVmNjAzNTA1MzEyMzQ1MDAzOTY1ZmYyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tCZ21xfS4oFnkXa17pWIkp-kV2XCJWcGm1CsPd8WYJk";
+const BASE_PATH = "https://api.themoviedb.org/3/";
+
+interface IMovie {
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export function getMovies() {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${BEARER_KEY}`,
+    },
+  };
+  return fetch(
+    `${BASE_PATH}/movie/now_playing?language=en-US&page=1`,
+    options
+  ).then((res) => res.json());
+}
